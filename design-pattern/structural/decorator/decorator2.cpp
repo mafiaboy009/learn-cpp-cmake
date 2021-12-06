efaaeb8919b46c9edd9ebd7ef873bb03bab28115
mafiaboy009 <ajaygopal.k@gmail.com>
2021-12-07 00:26:52 +0530

    design pattern decorator2 cleaned up

--- design-pattern/structural/decorator/decorator2.cpp
+++ design-pattern/structural/decorator/decorator2.cpp
@@ -1,78 +1,56 @@
-#include <iostream>
-#include <string>
-#include <memory>
-
-class Interface {
-	public:
-		virtual ~Interface() { }
-		virtual void write (std::string&) = 0;
-};
-
-class Core : public Interface {
-	public:
-	   	~Core() {std::cout << "Core destructor called.\n";}
-	   	virtual void write (std::string& text) override {};  // Do nothing.
-};
-
-class Decorator : public Interface {
-	private:
-	   	std::unique_ptr<Interface> interface;
-	public:
-	   	Decorator (std::unique_ptr<Interface> c) {interface = std::move(c);}
-	   	virtual void write (std::string& text) override {interface->write(text);}
-};
-
-class MessengerWithSalutation : public Decorator {
-	private:
-	   	std::string salutation;
-	public:
-	   	MessengerWithSalutation (std::unique_ptr<Interface> c, const std::string& str) : Decorator(std::move(c)), salutation(str) {}
-	   	~MessengerWithSalutation() {std::cout << "Messenger destructor called.\n";}
-	   	virtual void write (std::string& text) override {
-		   	text = salutation + "\n\n" + text;
-		 	Decorator::write(text);
-		}
-};
-
-class MessengerWithValediction : public Decorator {
-	private:
-	   	std::string valediction;
-	public:
-	   	MessengerWithValediction (std::unique_ptr<Interface> c, const std::string& str) : Decorator(std::move(c)), valediction(str) {}
-	   	~MessengerWithValediction() {std::cout << "MessengerWithValediction destructor called.\n";}
-	   	virtual void write (std::string& text) override {
-		 	Decorator::write(text);
-			text += "\n\n" + valediction;
-		}
-};
+#include "decorator2.hpp"
 
 int main() {
-	const std::string salutation = "Greetings,";
-	const std::string valediction = "Sincerly, Andy";
-	std::string message1 = "This message is not decorated.";
-	std::string message2 = "This message is decorated with a salutation.";
-	std::string message3 = "This message is decorated with a valediction.";
-	std::string message4 = "This message is decorated with a salutation and a valediction.";
 
-	std::unique_ptr<Interface> messenger1 = std::make_unique<Core>();
-	std::unique_ptr<Interface> messenger2 = std::make_unique<MessengerWithSalutation> (std::make_unique<Core>(), salutation);
-	std::unique_ptr<Interface> messenger3 = std::make_unique<MessengerWithValediction> (std::make_unique<Core>(), valediction);
-	std::unique_ptr<Interface> messenger4 = std::make_unique<MessengerWithValediction> (std::make_unique<MessengerWithSalutation>
-  						                  (std::make_unique<Core>(), salutation), valediction);
+	const string salutation = "Greetings,";
+	const string valediction = "Sincerly, Andy";
+
+	string message1 = "This message is not decorated.";
+	string message2 = "This message is decorated with a salutation.";
+	string message3 = "This message is decorated with a valediction.";
+	string message4 = "This message is decorated with a salutation and a valediction.";
+	string message5 = "This message is decorated with a salutation and a valediction.";
+
+	unique_ptr<Interface> messenger1 = make_unique<Core>();
+
+	unique_ptr<Interface> messenger2 = make_unique<MessengerWithSalutation> (make_unique<Core>(), salutation);
+
+	unique_ptr<Interface> messenger3 = make_unique<MessengerWithValediction> (make_unique<Core>(), valediction);
+
+	unique_ptr<Interface> messenger4 = 
+        make_unique<MessengerWithValediction>( 
+                make_unique<MessengerWithSalutation>( make_unique<Core>(), salutation ), 
+                valediction );
+
+	unique_ptr<Interface> messenger5 = 
+        make_unique<MessengerWithSalutation>( 
+                make_unique<MessengerWithValediction>( make_unique<Core>(), valediction ), 
+                salutation );
 	
+	cout << "\n------------start-------------\n\n";
 	messenger1->write(message1);
-	std::cout << message1 << '\n';
-	std::cout << "\n------------------------------\n\n";
+	cout << "message1\n" << message1 << '\n';
+	cout << "\n------------------------------\n\n";
 
+	cout << "\n------------start-------------\n\n";
 	messenger2->write(message2);
-	std::cout << message2 << '\n';
-	std::cout << "\n------------------------------\n\n";
+	cout << "message2\n" << message2 << '\n';
+	cout << "\n------------------------------\n\n";
 
+	cout << "\n------------start-------------\n\n";
 	messenger3->write(message3);
-	std::cout << message3 << '\n';
-	std::cout << "\n------------------------------\n\n";
+	cout << "message3\n" << message3 << '\n';
+	cout << "\n------------------------------\n\n";
 
+	cout << "\n------------start-------------\n\n";
 	messenger4->write(message4);
-	std::cout << message4 << '\n';
-	std::cout << "\n------------------------------\n\n";
+	cout << "message4\n" << message4 << '\n';
+	cout << "\n------------------------------\n\n";
+
+	cout << "\n------------start-------------\n\n";
+	messenger5->write(message5);
+	cout << "message5\n" << message5 << '\n';
+	cout << "\n------------------------------\n\n";
+
+    return 0;
 }
