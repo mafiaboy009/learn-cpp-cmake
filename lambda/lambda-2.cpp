e779001c0c9d5f661d59269245361350bce1cfad
mafiaboy009 <ajaygopal.k@gmail.com>
2021-12-09 16:21:24 +0530

    lambda added

---
+++ lambda/lambda-2.cpp
@@ -0,0 +1,28 @@
+#include "lambda.hpp"
+
+int main(){
+    int a{5};
+    char b{'c'};
+    string d{"lambda"};
+
+    auto function = [=]() mutable {
+        cout << "lamdba says" << newLine;
+        cout << "int " << a << newLine;
+        cout << "char " << b << newLine;
+        cout << "string " << d << newLine;
+        a = 10;
+        b = 'd';
+        d = "expression";
+    };
+
+    function();
+
+    cout << string( 10, '*') << newLine;
+    cout << "main says" << newLine;
+    cout << "int " << a << newLine;
+    cout << "char " << b << newLine;
+    cout << "string " << d << newLine;
+
+    return 0;
+}
+
