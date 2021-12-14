696b0112ff37cb5e127d83ad53c7a659b00f779d
mafiaboy009 <ajaygopal.k@gmail.com>
2021-12-15 00:57:58 +0530

    interview-question updated

---
+++ interview-question/sort-array-of-2-different-numbers/sort-array-of-2-different-numbers01.cpp
@@ -0,0 +1,46 @@
+/*
+ * An array has two types of elements only, viz., 0 and 1
+ * Sort the array in O(n)
+ *
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void displayArray(const array<int, 11>& boolArray){
+    for( auto& a: boolArray ){
+        cout << a << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+    const int size{11};
+    array<int, size> boolArray = {1,0,1,0,1,1,0,1,1,0,1};
+    //array<int, size> boolArray = {1,1,1,1,1,1,1,1,1,1,1};
+    //array<int, size> boolArray = {0,0,0,0,0,0,0,0,0,0,0};
+
+    displayArray( boolArray );
+
+    int countZero{0};
+    for(int i=0; i < size; ++i ){ // O(n)
+        if( boolArray[i] == 0 ){
+            ++countZero;
+        }
+    }
+
+    for(int i=0; i<countZero; ++i){ // O(n)
+        boolArray[i]=0;
+    }
+
+    for(int i=countZero; i<size; ++i){ // O(1)
+        boolArray[i]=1;
+    }
+
+    displayArray( boolArray );
+
+    return 0;
+}
---
+++ interview-question/sort-array-of-2-different-numbers/sort-array-of-2-different-numbers02.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    const int size{11};
+    array<int, size> boolArray = {1,0,1,0,1,1,0,1,1,0,1};
+    //array<int, size> boolArray = {1,1,1,1,1,1,1,1,1,1,1};
+    //array<int, size> boolArray = {0,0,0,0,0,0,0,0,0,0,0};
+
+    for( auto& a: boolArray ){
+        cout << a << " ";
+    }
+    cout << endl;
+
+    /*
+    // bubble sort; O(n^2)
+    for( int i=0; i<size; ++i ){
+        for( int j=0; j < size-1; ++j ){
+            if( boolArray[j] > boolArray[j+1]){
+                swap( boolArray[j], boolArray[j+1]);
+            }
+        }
+    }
+    */
+    
+    // O(n); 
+    int i{0}, j{size-1};
+    int foundI{-1}, foundJ{-1};
+    while( i < j ){
+
+        bool foundZero = false;
+        bool foundOne = false;
+
+        while( j > 0 ){
+            if( boolArray[j] == 0 ){
+                foundJ = j;
+                foundZero = true;
+                --j;
+                break;
+            }
+            --j;
+        }
+
+        while( i < size ){
+            if( boolArray[i] == 1 ){
+                foundI = i;
+                foundOne = true;
+                ++i;
+                break;
+            }
+            ++i;
+        }
+
+        if( foundZero && foundOne && i < j ){
+            swap( boolArray[foundI], boolArray[foundJ] );
+        }
+
+    }
+
+    cout << "foundI " << foundI << endl;
+    cout << "foundJ " << foundJ << endl;
+
+    for( auto& a: boolArray ){
+        cout << a << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
