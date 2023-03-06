919363665a1fbed211ea9d513984c6d45089ebbd
mafiaboy009 <ajaygopal.k@gmail.com>
2023-03-06 18:31:27 +0530

    updated algorithm random1

--- algorithm/random/random-1.cpp
+++ algorithm/random/random-1.cpp
@@ -8,10 +8,13 @@ int main(){
 	std::mt19937 engine{seed()};
 	std::uniform_int_distribution<> dis{1, 10};
 
+    std::default_random_engine defaultRandomEngine;
+
 	int i{0};
 	while( i < 6 ){
 		int a{ dis(engine) };
-		std::cout << a << '\n';
+		std::cout << "default : " << defaultRandomEngine()%10 << '\n';
+		std::cout << "mt19937 : " << a << '\n';
 		++i;
 	}
 
